6a144952c2acd5c730a8bcfa88a188d2547370a1
patrick-kasl <pkasl@ucsd.edu>
2023-01-29 17:20:44 -0800

    film photos

--- fetch.js
+++ fetch.js
@@ -20,16 +20,16 @@ var mapbox_features = [];
 
 // 
 function trolley_geojson(lon, lat, direction) {
-  
-  if (direction == 'North'){
+
+  if (direction == 'North') {
     console.log(direction);
     return {
       // feature for Mapbox DC
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [lon,lat]
-        },
+        'coordinates': [lon, lat]
+      },
       'properties': {
         'direction': direction
       }
@@ -40,38 +40,38 @@ function trolley_geojson(lon, lat, direction) {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [lon,lat]
-        },
+        'coordinates': [lon, lat]
+      },
       'properties': {
         'direction': direction
       }
     };
   }
-  
+
 }
 
-d3.json("./trips_test.json", function(data){
-    trips = data;
+d3.json("./trips_test.json", function (data) {
+  trips = data;
 
-  d3.json("./blue_line_shape.json", function(data){
+  d3.json("./blue_line_shape.json", function (data) {
     shapes = data;
     console.log(shapes);
 
-(async () => {
+    (async () => {
 
-try {
-    const response = await fetch("https://realtime.sdmts.com/api/api/gtfs_realtime/vehicle-positions-for-agency/MTS.pb?key=e2f3da8d-2ea3-40cf-9e30-0b937f0e3817", {
-    
-    });
-    
-    const buffer = await response.arrayBuffer();
-    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
-    new Uint8Array(buffer)
-    );
+      try {
+        const response = await fetch("https://realtime.sdmts.com/api/api/gtfs_realtime/vehicle-positions-for-agency/MTS.pb?key=e2f3da8d-2ea3-40cf-9e30-0b937f0e3817", {
 
-    feed.entity.forEach(function(entity) {
-        if (entity.vehicle.trip != null){
-            if (entity.vehicle.trip.routeId == '510'){
+        });
+
+        const buffer = await response.arrayBuffer();
+        const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+          new Uint8Array(buffer)
+        );
+
+        feed.entity.forEach(function (entity) {
+          if (entity.vehicle.trip != null) {
+            if (entity.vehicle.trip.routeId == '510') {
 
               latitude.push(entity.vehicle.position.latitude);
               longitude.push(entity.vehicle.position.longitude);
@@ -79,86 +79,86 @@ try {
               directions.push(trips[entity.vehicle.trip.tripId].direction_name);
               mapbox_features.push(trolley_geojson(entity.vehicle.position.longitude, entity.vehicle.position.latitude, trips[entity.vehicle.trip.tripId].direction_name));
             }
+          }
+        });
+
+      }
+      catch (error) {
+        console.log(error);
+        process.exit(1);
+      }
+    })();
+
+
+    var mapbox_js_shape = [];
+
+    //Get the route line points in the right structure
+    for (let i = 0; i < shapes['0'].length; i++) {
+      mapbox_js_shape.push([shapes[1][i], shapes[0][i]]);
+    }
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGthc2wiLCJhIjoiY2xkOWswampzMDl0bTNubW0zaGZwa3JudSJ9.rfPdeEe_uLSGhWcRZbbhpA';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+      style: 'mapbox://styles/pkasl/cldhqqkmp000z01pf374os34v',
+      center: [-117.138044, 32.716734],
+      zoom: 10
+    });
+
+    map.on('load', () => {
+      map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+            'type': 'LineString',
+            'coordinates': mapbox_js_shape
+          }
         }
-    });
-   
-}
-catch (error) {
-    console.log(error);
-    process.exit(1);
-}
-})(); 
+      });
+      map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': '#0063a7',
+          'line-width': 5
+        }
+      });
+      map.addSource('trolley_locations', {
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'properties': {},
+          'features': mapbox_features,
+        }
+      });
 
-
-var mapbox_js_shape = [];
-
-//Get the route line points in the right structure
-for (let i = 0; i < shapes['0'].length; i++){
-  mapbox_js_shape.push([shapes[1][i], shapes[0][i]]);
-}
-
-mapboxgl.accessToken = 'pk.eyJ1IjoicGthc2wiLCJhIjoiY2xkOWswampzMDl0bTNubW0zaGZwa3JudSJ9.rfPdeEe_uLSGhWcRZbbhpA';
-const map = new mapboxgl.Map({
-  container: 'map',
-  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-  style: 'mapbox://styles/pkasl/cldhqqkmp000z01pf374os34v',
-  center: [-117.138044, 32.716734],
-  zoom: 10
-});
- 
-map.on('load', () => {
-  map.addSource('route', {
-    'type': 'geojson',
-    'data': {
-      'type': 'Feature',
-      'properties': {},
-      'geometry': {
-        'type': 'LineString',
-        'coordinates': mapbox_js_shape
-      }
-    }
-  });
-  map.addLayer({
-    'id': 'route',
-    'type': 'line',
-    'source': 'route',
-    'layout': {
-      'line-join': 'round',
-      'line-cap': 'round'
-    },
-    'paint': {
-      'line-color': '#0063a7',
-      'line-width': 5
-    }
-  });
-  map.addSource('trolley_locations', {
-    'type': 'geojson',
-    'data': {
-      'type': 'FeatureCollection',
-      'properties': {},
-      'features': mapbox_features,
-      }
-    });
-
-  map.addLayer({
-    'id': 'trolley_locations',
-    'type': 'circle',
-    'source': 'trolley_locations',
-    'paint': {
-      'circle-radius': 7,
-      'circle-stroke-width': 1,
-      'circle-color': [
-        'match',
-        ['get', 'direction'],
-        'North', '#02ffc4',
-        'South', '#dd02ff',
+      map.addLayer({
+        'id': 'trolley_locations',
+        'type': 'circle',
+        'source': 'trolley_locations',
+        'paint': {
+          'circle-radius': 7,
+          'circle-stroke-width': 1,
+          'circle-color': [
+            'match',
+            ['get', 'direction'],
+            'North', '#02ffc4',
+            'South', '#dd02ff',
         /*else */ '#ccc'
-        ],
-      
-      'circle-stroke-color': 'white'
-    }
-  });
-});
+          ],
 
-});
+          'circle-stroke-color': 'white'
+        }
+      });
+    });
+
+  });
 });
