0c425e0ecceed34218de45cfbfda798ac4155076
patrick-kasl <pkasl@ucsd.edu>
2023-03-28 20:06:04 -0700

    a bit of an aggressive strategy, but might load on first try each time now

--- bundle_fetch.js
+++ bundle_fetch.js
@@ -72,26 +72,26 @@ function changeRoute(choice) {
 // This allows the function to be used by the button in HTML
 window.changeRoute = changeRoute;
 
-function tableTitleDirection(route_color, direction){
+function tableTitleDirection(route_color, direction) {
     var titleDirection;
-    
+
     if (route_color == "Blue") {
-        if (direction == 0){
+        if (direction == 0) {
             titleDirection = "North";
             //document.getElementById("direction_name_table").innerHTML = "North";
         }
 
-        if (direction == 1){
+        if (direction == 1) {
             titleDirection = "South";
             //document.getElementById("direction_name_table").innerHTML = "South";
         }
     } else {
-        if (direction == 0){
+        if (direction == 0) {
             titleDirection = "East";
             //document.getElementById("direction_name_table").innerHTML = "East";
         }
 
-        if (direction == 1){
+        if (direction == 1) {
             titleDirection = "West";
             //document.getElementById("direction_name_table").innerHTML = "West";
         }
@@ -104,7 +104,7 @@ function changeDirection(choice) {
 
     document.getElementById("direction_name_table").innerHTML = tableTitleDirection(route_color, direction);
 
-    arrival_time_out  = mts_predicted_arrival_times(route_color);
+    arrival_time_out = mts_predicted_arrival_times(route_color);
     const table_body = document.getElementById("table_body");
     table_body.innerHTML = tableHTML(arrival_time_out, direction);
 
@@ -114,7 +114,7 @@ function changeDirection(choice) {
 window.changeDirection = changeDirection;
 
 
-function tableHTML(arrival_time_out, direction){
+function tableHTML(arrival_time_out, direction) {
 
     hmtl_holder = [];
     //console.log(arrival_time_out);
@@ -123,9 +123,9 @@ function tableHTML(arrival_time_out, direction){
         hmtl_holder.push(`<tr>`);
         hmtl_holder.push(`<th scope="row" style = "font-size: 0.85em;">${stop}</th>`);
         //table_body.innerHTML += ;
-        
+
         let temp = arrival_time_out[direction].get(stop);
-        for (let i=0;i<=2;i++){
+        for (let i = 0; i <= 2; i++) {
             hmtl_holder.push(`<td>${temp[i]}</td>`);
             //table_body.innerHTML += `<td>${temp[i]}</td>`;
         };
@@ -154,122 +154,131 @@ function trolley_geojson(lon, lat, direction) {
 };
 
 async function mts_predicted_arrival_times(route_color) {
+    let success = false;
+    while (!success) {
 
-    try {
-        const response = await fetch("https://realtime.sdmts.com/api/api/gtfs_realtime/trip-updates-for-agency/MTS.pb?key=e2f3da8d-2ea3-40cf-9e30-0b937f0e3817", {});
 
-        const buffer = await response.arrayBuffer();
-        const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
-            new Uint8Array(buffer)
-        );
+        try {
+            const response = await fetch("https://realtime.sdmts.com/api/api/gtfs_realtime/trip-updates-for-agency/MTS.pb?key=e2f3da8d-2ea3-40cf-9e30-0b937f0e3817", {});
+            success = true;
+            const buffer = await response.arrayBuffer();
+            const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+                new Uint8Array(buffer)
+            );
 
-        // These will store the predicted times from the most recent feed of trip updates
-        let stop_times_direction_one = new Map();
-        let stop_times_direction_two = new Map();
+            // These will store the predicted times from the most recent feed of trip updates
+            let stop_times_direction_one = new Map();
+            let stop_times_direction_two = new Map();
 
-        
-        stop_names.stops[route_color].forEach(function (stop) {
-            stop_times_direction_one.set(stop, []);
-            stop_times_direction_two.set(stop, []);
-        });
-        
-        feed.entity.forEach(function (entity) {
-            if (entity.tripUpdate.trip != null) {
-                if (entity.tripUpdate.trip.routeId == color_to_route_id[route_color]) {
-                    if (route_color == "Blue") {
-                        if (trips[entity.tripUpdate.trip.tripId].direction_name == 'North') {
-                            entity.tripUpdate.stopTimeUpdate.forEach(function (stop_time_update) {
-                                let test = stop_times_direction_one.get(stop_id_to_name[stop_time_update.stopId]);
-                                test.push(Math.round((stop_time_update.arrival.time*1000 - Date.now())/(1000*60)));
-                            });
+
+            stop_names.stops[route_color].forEach(function (stop) {
+                stop_times_direction_one.set(stop, []);
+                stop_times_direction_two.set(stop, []);
+            });
+
+            feed.entity.forEach(function (entity) {
+                if (entity.tripUpdate.trip != null) {
+                    if (entity.tripUpdate.trip.routeId == color_to_route_id[route_color]) {
+                        if (route_color == "Blue") {
+                            if (trips[entity.tripUpdate.trip.tripId].direction_name == 'North') {
+                                entity.tripUpdate.stopTimeUpdate.forEach(function (stop_time_update) {
+                                    let test = stop_times_direction_one.get(stop_id_to_name[stop_time_update.stopId]);
+                                    test.push(Math.round((stop_time_update.arrival.time * 1000 - Date.now()) / (1000 * 60)));
+                                });
+                            }
+                            if (trips[entity.tripUpdate.trip.tripId].direction_name == 'South') {
+                                entity.tripUpdate.stopTimeUpdate.forEach(function (stop_time_update) {
+                                    let test = stop_times_direction_two.get(stop_id_to_name[stop_time_update.stopId]);
+                                    test.push(Math.round((stop_time_update.arrival.time * 1000 - Date.now()) / (1000 * 60)));
+                                });
+                            }
+                        } else {
+                            if (trips[entity.tripUpdate.trip.tripId].direction_name == 'West') {
+                                entity.tripUpdate.stopTimeUpdate.forEach(function (stop_time_update) {
+                                    let test = stop_times_direction_one.get(stop_id_to_name[stop_time_update.stopId]);
+                                    test.push(Math.round((stop_time_update.arrival.time * 1000 - Date.now()) / (1000 * 60)));
+                                });
+                            }
+                            if (trips[entity.tripUpdate.trip.tripId].direction_name == 'East') {
+                                entity.tripUpdate.stopTimeUpdate.forEach(function (stop_time_update) {
+                                    let test = stop_times_direction_two.get(stop_id_to_name[stop_time_update.stopId]);
+                                    test.push(Math.round((stop_time_update.arrival.time * 1000 - Date.now()) / (1000 * 60)));
+                                });
+                            }
+
                         }
-                        if (trips[entity.tripUpdate.trip.tripId].direction_name == 'South') {
-                            entity.tripUpdate.stopTimeUpdate.forEach(function (stop_time_update) {
-                                let test = stop_times_direction_two.get(stop_id_to_name[stop_time_update.stopId]);
-                                test.push(Math.round((stop_time_update.arrival.time*1000 - Date.now())/(1000*60)));
-                            });
-                        }
-                    } else {
-                        if (trips[entity.tripUpdate.trip.tripId].direction_name == 'West') {
-                            entity.tripUpdate.stopTimeUpdate.forEach(function (stop_time_update) {
-                                let test = stop_times_direction_one.get(stop_id_to_name[stop_time_update.stopId]);
-                                test.push(Math.round((stop_time_update.arrival.time*1000 - Date.now())/(1000*60)));
-                            });
-                        }
-                        if (trips[entity.tripUpdate.trip.tripId].direction_name == 'East') {
-                            entity.tripUpdate.stopTimeUpdate.forEach(function (stop_time_update) {
-                                let test = stop_times_direction_two.get(stop_id_to_name[stop_time_update.stopId]);
-                                test.push(Math.round((stop_time_update.arrival.time*1000 - Date.now())/(1000*60)));
-                            });
-                        }
-
                     }
+
                 }
 
-            }
-        
-        });
+            });
 
-        stop_names.stops[route_color].forEach(function (stop) {
-            stop_times_direction_one.set(stop, stop_times_direction_one.get(stop).sort(function(a, b){return a-b}));
-            stop_times_direction_one.set(stop, stop_times_direction_one.get(stop).filter( x => x > -0 ));
-            stop_times_direction_one.set(stop, stop_times_direction_one.get(stop).filter( function( item, index, inputArray ) {return inputArray.indexOf(item) == index}));
+            stop_names.stops[route_color].forEach(function (stop) {
+                stop_times_direction_one.set(stop, stop_times_direction_one.get(stop).sort(function (a, b) { return a - b }));
+                stop_times_direction_one.set(stop, stop_times_direction_one.get(stop).filter(x => x > -0));
+                stop_times_direction_one.set(stop, stop_times_direction_one.get(stop).filter(function (item, index, inputArray) { return inputArray.indexOf(item) == index }));
 
-            stop_times_direction_two.set(stop, stop_times_direction_two.get(stop).sort(function(a, b){return a-b}));
-            stop_times_direction_two.set(stop, stop_times_direction_two.get(stop).filter( x => x > -0 ));
-            stop_times_direction_two.set(stop, stop_times_direction_two.get(stop).filter( function( item, index, inputArray ) {return inputArray.indexOf(item) == index}));
-        });
+                stop_times_direction_two.set(stop, stop_times_direction_two.get(stop).sort(function (a, b) { return a - b }));
+                stop_times_direction_two.set(stop, stop_times_direction_two.get(stop).filter(x => x > -0));
+                stop_times_direction_two.set(stop, stop_times_direction_two.get(stop).filter(function (item, index, inputArray) { return inputArray.indexOf(item) == index }));
+            });
 
-        stop_names.stops[route_color].forEach(function (stop) {
-            let test = stop_times_direction_one.get(stop);
-            test.push('*');
-            test.push('*');
-            test.push('*'); 
-            
-            let test_2 = stop_times_direction_two.get(stop);
-            test_2.push('*');
-            test_2.push('*');
-            test_2.push('*'); 
+            stop_names.stops[route_color].forEach(function (stop) {
+                let test = stop_times_direction_one.get(stop);
+                test.push('*');
+                test.push('*');
+                test.push('*');
 
-        });
+                let test_2 = stop_times_direction_two.get(stop);
+                test_2.push('*');
+                test_2.push('*');
+                test_2.push('*');
 
-        
-        return [stop_times_direction_one, stop_times_direction_two];
-    }
-    catch (error) {
-        console.log(error);
-        //process.exit(1);
-    }
+            });
+
+
+            return [stop_times_direction_one, stop_times_direction_two];
+        }
+        catch (error) {
+            console.log(error);
+            //process.exit(1);
+        }
+    };
 };
 
 async function getLocation(route_color) {
-    try {
-        const response = await fetch("https://realtime.sdmts.com/api/api/gtfs_realtime/vehicle-positions-for-agency/MTS.pb?key=e2f3da8d-2ea3-40cf-9e30-0b937f0e3817", {});
+    let success = false;
+    while (!success) {
 
-        const buffer = await response.arrayBuffer();
-        const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
-            new Uint8Array(buffer)
-        );
+        try {
 
-        var mapbox_features = [];
-        feed.entity.forEach(function (entity) {
-            if (entity.vehicle.trip != null) {
-                if (entity.vehicle.trip.routeId == color_to_route_id[route_color]) {
-                    mapbox_features.push(trolley_geojson(entity.vehicle.position.longitude, entity.vehicle.position.latitude, trips[entity.vehicle.trip.tripId].direction_name));
+            const response = await fetch("https://realtime.sdmts.com/api/api/gtfs_realtime/vehicle-positions-for-agency/MTS.pb?key=e2f3da8d-2ea3-40cf-9e30-0b937f0e3817", {});
+            success = true;
+            const buffer = await response.arrayBuffer();
+            const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+                new Uint8Array(buffer)
+            );
+
+            var mapbox_features = [];
+            feed.entity.forEach(function (entity) {
+                if (entity.vehicle.trip != null) {
+                    if (entity.vehicle.trip.routeId == color_to_route_id[route_color]) {
+                        mapbox_features.push(trolley_geojson(entity.vehicle.position.longitude, entity.vehicle.position.latitude, trips[entity.vehicle.trip.tripId].direction_name));
+                    }
                 }
-            }
-        });
-        updated_time = new Date(feed.header.timestamp * 1000);
-        // We're at an eight hour offset from UTC: 8*60*60
-        //(updated_time);
-        document.getElementById("last_updated").innerHTML = updated_time.toLocaleString();
+            });
+            updated_time = new Date(feed.header.timestamp * 1000);
+            // We're at an eight hour offset from UTC: 8*60*60
+            //(updated_time);
+            document.getElementById("last_updated").innerHTML = updated_time.toLocaleString();
 
-        return mapbox_features;
-    }
-    catch (error) {
-        console.log(error);
-        //process.exit(1);
-    }
+            return mapbox_features;
+        }
+        catch (error) {
+            console.log(error);
+            //process.exit(1);
+        }
+    };
 };
 
 
@@ -298,6 +307,9 @@ d3.json("./trips_test.json", function (data) {
 
                 map.on('load', async () => {
                     // Get the locations of all Vehicle positions.
+                    arrival_time_out = await mts_predicted_arrival_times(route_color);
+                    const table_body = document.getElementById("table_body");
+                    table_body.innerHTML = tableHTML(arrival_time_out, direction);
 
                     // Add a Blue line for the Blue line route
                     // This won't change on refresh
@@ -329,10 +341,8 @@ d3.json("./trips_test.json", function (data) {
 
                     const mapbox_features = await getLocation(route_color);
 
-                    arrival_time_out  = await mts_predicted_arrival_times(route_color);
-                    const table_body = document.getElementById("table_body");
-                    table_body.innerHTML = tableHTML(arrival_time_out, direction);
-                    
+
+
                     map.addSource('trolley_locations', {
                         'type': 'geojson',
                         'data': {
@@ -375,7 +385,7 @@ d3.json("./trips_test.json", function (data) {
                             }
                         );
 
-                        arrival_time_out  = await mts_predicted_arrival_times(route_color);
+                        arrival_time_out = await mts_predicted_arrival_times(route_color);
                         const table_body = document.getElementById("table_body");
                         table_body.innerHTML = tableHTML(arrival_time_out, direction);
 
